0187264a64897e4ca794cb54b7844541651efe71
navabiradar <nava526@outlook.com>
2024-06-28 21:50:59 +0530

    Updated Urls

--- src/components/ListUrls.svelte
+++ src/components/ListUrls.svelte
@@ -1,18 +1,151 @@
 <script type="module" >
 
-    export const FinanceUrls = {
-        fixedDeposit: {
-            name : "Fixed Deposit Calculator",
-            urls : "/finance/fixed-deposit-calculator",
+    export const urlsName = {
+       finance : [
+        {
+            name: "Fixed Deposit Calculator",
+            url: "/finance/fixed-deposit-calculator",
+            status: true,
         },
-        recurringDeposit: {
-            name : "Recurring Deposit Calculator",
-            urls : "/finance/fixed-deposit-calculator",
+        {
+            name: "Compound Interest Calculator",
+            url: "/finance/compound-interest-calculator",
+            status: false,
         },
-        InvestmentCalculator: {
-            name : "Investment  Calculator",
-            urls : "/finance/Investment-calculator",
+        {
+            name: "Recurring Deposit Calculator",
+            url: "/finance/recurring-deposit-calculator",
+            status: true,
         },
+        {
+            name: "Investment Calculator",
+            url: "/finance/investment-calculator",
+            status: true,
+        },
+        {
+            name: "Future Value Calculator",
+            url: "/finance/future-value-calculator",
+            status: true,
+        },
+        {
+            name: "Compound Annual Growth Rate (CAGR) Calculator",
+            url: "/finance/cagr-calculator",
+            status: false,
+        },
+        {
+            name: "National Pension System(NPS) Calculator",
+            url: "/finance/nps-calculator",
+            status: false,
+        },
+        {
+            name: "Interest Calculator",
+            url: "/finance/interest-calculator",
+            status: false,
+        },
+        {
+            name: "Payment Calculator",
+            url: "/finance/payment-calculator",
+            status: false,
+        },
+        {
+            name: "Retirement Calculator",
+            url: "/finance/retirement-calculator",
+            status: false,
+        },
+        {
+            name: "Systematic Investment Plan (SIP) Calculator",
+            url: "/finance/sip-calculator",
+            status: false,
+        },
+        {
+            name: "Stepup SIP Calculator",
+            url: "/finance/stepup-sip-calculator",
+            status: false,
+        },
+        {
+            name: "Systematic Withdrawal Plan (SWP) Calculator",
+            url: "/finance/swp-calculator",
+            status: false,
+        },
+        {
+            name: "Loan Calculator",
+            url: "/finance/loan-calculator",
+            status: false,
+        },
+        {
+            name: "Home Loan Calculator",
+            url: "/finance/home-loan-calculator",
+            status: false,
+        },
+        {
+            name: "Personal Loan Calculator",
+            url: "/finance/personal-loan-calculator",
+            status: false,
+        },
+        {
+            name: "Salary Calculator",
+            url: "/finance/salary-calculator",
+            status: false,
+        },
+        {
+            name: "Mortgage Calculator",
+            url: "/finance/mortgage-calculator",
+            status: false,
+        },
+        {
+            name: "Auto Loan Calculator",
+            url: "/finance/auto-loan-calculator",
+            status: false,
+        },
+        {
+            name: "Amortization Calculator",
+            url: "/finance/amortization-calculator",
+            status: false,
+        },
+
+       ],
+       health : [
+        {
+            name: "Body Mass Index(BMI) Calculator",
+            url: "/health/bmi-calculator",
+            status: true,
+        },
+        {
+            name: "Body Fat Calculator",
+            url: "/health/body-fat-calculator",
+            status: true,
+        },
+        {
+            name: "Ideal Weight Calculator",
+            url: "/health/body-fat-calculator",
+            status: true,
+        },
+        {
+            name: "PACE Calculator",
+            url: "/health/pace-calculator",
+            status: true,
+        },
+        {
+            name: "Army Body Fat Calculator",
+            url: "/health/army-body-fat-calculator",
+            status: true,
+        },
+        {
+            name: "Calorie Calculator",
+            url: "/health/calorie-calculator",
+            status: true,
+        },
+        {
+            name: "Health Weight Calculator",
+            url: "/health/healthy-weight-calculator",
+            status: true,
+        },
+        {
+            name: "Calories Burned Calculator",
+            url: "/health/calories-burned-calculator",
+            status: true,
+        },
+       ]
 
     }
 
